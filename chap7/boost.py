711233bf81fe09690cbf1d1296dbb5899719572a
meloncoder <melonCoder@126.com>
2017-01-01 23:39:50 -0800

    Add chap7
    - From now on, python3 is the main complier.
    - Added chapter 7.

--- chap7/boost.py
+++ chap7/boost.py
@@ -12,9 +12,10 @@ def loadSimpleData():
 def stumpClassify(dataMatrix, dimen, threshVal, thresIneq):
     retArray = ones((shape(dataMatrix)[0], 1))
     if thresIneq == 'lt':
-        retArray[dataMatrix[:, dimen] < threshVal] = -1.0
+        retArray[dataMatrix[:, dimen] <= threshVal] = -1.0
     else:
         retArray[dataMatrix[:, dimen] > threshVal] = -1.0
+    return retArray
 
 def buildStump(dataArr, classLabels, D):
     dataMatrix = mat(dataArr)
@@ -37,9 +38,9 @@ def buildStump(dataArr, classLabels, D):
                 errArr = mat(ones((m, 1)))
                 errArr[predictedVals == labelMat] = 0
                 weightedError = D.T * errArr
-                print "split: dim %d, thresh: %.2f, thresh inequal: %s\
+                print("split: dim %d, thresh: %.2f, thresh inequal: %s\
                         the weighted error is %.3f" \
-                        % (i, threshVal, inequal, weightedError)
+                        % (i, threshVal, inequal, weightedError))
                 if weightedError < minError:
                     minError = weightedError
                     bestClassEst = predictedVals.copy()
@@ -47,3 +48,88 @@ def buildStump(dataArr, classLabels, D):
                     bestStump['thresh'] = threshVal
                     bestStump['ineq'] = inequal
     return bestStump, minError, bestClassEst
+
+def adaBoostTrainDS(dataArr, classLabels, numIt = 40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m, 1)) / m)
+    aggClassEst = mat(zeros((m, 1)))
+    for i in  range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print("D: ", D.T)
+        alpha = float(0.5 * log((1.0 - error) / max(error, 1e-16)))
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)
+        print("classEst: ", classEst.T)
+        
+        expon = multiply(-1 * alpha * mat(classLabels).T, classEst)
+        D = multiply(D, exp(expon))
+        D = D / D.sum()
+        aggClassEst += alpha * classEst
+        print("aggclassEst: ", aggClassEst.T)
+        aggErrors = multiply(sign(aggClassEst) !=  mat(classLabels).T, \
+            ones((m, 1)))
+        errorRate = aggErrors.sum() / m
+        print("total error is: ", errorRate, "\n")
+        if errorRate == 0.0:
+            break;
+    return weakClassArr, aggClassEst
+
+def adaClassify(datToClass, classifierArr):
+    dataMatrix = mat(datToClass)
+    m = shape(dataMatrix)[0]
+    aggClassEst = mat(zeros((m, 1)))
+
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'], \
+                classifierArr[i]['thresh'], classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha'] * classEst
+        print(aggClassEst)
+    
+    return sign(aggClassEst)
+
+def loadDataSet(fileName):
+    numFeat = len(open(fileName).readline().split('\t'))
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat - 1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+
+def plotROC(predStrengths, classLabels):
+    import matplotlib.pyplot as plt
+    cur = (1.0, 1.0)
+    ySum = 0.0
+    numPosClas = sum(array(classLabels) == 1.0)
+    yStep = 1 / float(numPosClas)
+    xSetp = 1 / float(len(classLabels) - numPosClas)
+    sortedIndices = predStrengths.argsort()
+
+    fig = plt.figure()
+    fig.clf()
+    ax = plt.subplot(111)
+
+    for index in sortedIndices.tolist()[0]:
+        if classLabels[index] == 1.0:
+            delX = 0
+            delY = yStep
+        else:
+            delX = xStep
+            delY = 0
+            ySum += cur[1]
+        ax.plot([cur[0], cur[0] - delX], [cur[1], cur[1] - delY], c='b')
+        cur = (cur[0] - delX, cur[1] - delY)
+    ax.plot([0, ], [0, 1], 'b-.-')
+    plt.xlabel('false positive rate')
+    plt.ylabel('true positive rate')
+    plt.title('ROC curve for adaboost horse colic detection system')
+    ax.axis([0, 1, 0, 1])
+    plt.show()
+    print("the area under the curve is ", ySum * xStep)
