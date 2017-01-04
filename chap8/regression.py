4278562e2b5bf2330068c64b7a40cc1d2d76c54f
meloncoder <melonCoder@126.com>
2017-01-04 08:54:38 -0800

    add ridge regression and stagewise regression

--- chap8/regression.py
+++ chap8/regression.py
@@ -22,7 +22,7 @@ def standRegres(xArr, yArr):
 
     xTx = xMat.T * xMat
     if linalg.det(xTx) == 0.0:
-        print("This matrix is sigular, con't do inverse")
+        print("This matrix is sigular, can't do inverse")
         return
 
     ws = xTx.I * (xMat.T * yMat)
@@ -39,7 +39,7 @@ def lwlr(testPoint, xArr, yArr, k = 1.0):
         weights[j, j] = exp(diffMat * diffMat.T / (-2.0 * k**2))
     xTx = xMat.T * (weights * xMat)
     if linalg.det(xTx) == 0.0:
-        print("This matrix is sigular, con't do inverse")
+        print("This matrix is sigular, can't do inverse")
         return
 
     ws = xTx.I * (xMat.T * (weights * yMat))
@@ -54,3 +54,67 @@ def lwlrTest(testArr, xArr, yArr, k = 1.0):
 
 def rssError(yArr, yHatArr):
     return ((yArr- yHatArr)**2).sum()
+
+def ridgeRegres(xMat, yMat, lam = 0.2):
+    xTx = xMat.T * xMat
+    denom = xTx + eye(shape(xMat)[1]) * lam
+
+    if linalg.det(denom) == 0.0:
+        print("This matrix is sigular, can't do inverse")
+        return
+
+    ws = denom.I * (xMat.T * yMat)
+    return ws
+
+def ridgeTest(xArr, yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMeans = mean(xMat, 0)
+    xVar = var(xMat, 0)
+    xMat = (xMat - xMeans) / xVar
+    numTestPts = 30
+    print(shape(xMat))
+    print(shape(yMat))
+
+    wMat = zeros((numTestPts, shape(xMat)[1]))
+    for i in range(numTestPts):
+        ws = ridgeRegres(xMat, yMat, exp(i - 10))
+        wMat[i, :] = ws.T
+    return wMat
+
+def regularize(xMat):
+    inMat = xMat.copy()
+    inMeans = mean(inMat, 0)
+    inVar = var(inMat, 0)
+    inMat = (inMat - inMeans) / inVar
+    return inMat
+
+def stageWise(xArr, yArr, eps = 0.01, numIt = 100):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+    m, n = shape(xMat)
+    returnMat = zeros((numIt, n))
+    ws = zeros((n, 1))
+    wsTest = ws.copy()
+    wsMax = ws.copy()
+
+    for i in range(numIt):
+        print(ws.T)
+        lowestError = inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps * sign
+                yTest = xMat * wsTest
+                rssE = rssError(yMat.A, yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        returnMat[i, :] = ws.T
+    return returnMat
