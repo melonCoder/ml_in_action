64012d63149baa0a42d4ec7b7d082c6afb1e9fa6
meloncoder <melonCoder@126.com>
2017-01-10 09:33:33 -0800

    add post-prunc and model-tree

--- chap9/regTrees.py
+++ chap9/regTrees.py
@@ -10,7 +10,7 @@ def loadDataSet(fileName):
         dataMat.append(fltLine)
     return dataMat
 
-def plotData(fileName, i, j):
+def plotData(fileName, i = 0, j = 1):
     dataSet = loadDataSet(fileName)
     dataMat = mat(dataSet)
 
@@ -31,7 +31,29 @@ def regLeaf(dataSet):
 def regErr(dataSet):
     return var(dataSet[:, -1]) * shape(dataSet)[0]
 
-# creatrTree is not compliable until regLeaf, regErr and chooseBestSplit is
+def linearSolve(dataSet):
+    m, n = shape(dataSet)
+    X = mat(ones((m, n)))
+    Y = mat(ones((m, 1)))
+    X[:, 1 : n] = dataSet[:, 0 : n - 1]
+    Y = dataSet[:, -1]
+    xTx = X.T*X
+    if linalg.det(xTx) == 0.0:
+        raise NameError('This matrix is singular, can not do inverse,\n\
+                try increase the second value of ops')
+    ws = xTx.I * (X.T * Y)
+    return ws, X, Y
+
+def modelLeaf(dataSet):
+    ws, X, Y = linearSolve(dataSet)
+    return ws
+
+def modelErr(dataSet):
+    ws, X, Y = linearSolve(dataSet)
+    yHat = X * ws
+    return sum(power(Y - yHat, 2))
+
+# createTree is not compliable until regLeaf, regErr and chooseBestSplit is
 # defined.
 def createTree(dataSet, leafType = regLeaf, errType = regErr, ops = [1, 4]):
     feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
@@ -52,7 +74,7 @@ def chooseBestSplit(dataSet, leafType = regLeaf, errType = regErr, ops = [1,
     tolN = ops[1]
     if len(set(dataSet[:, -1].T.tolist()[0])) == 1:
         return None, leafType(dataSet)
-    
+
     m, n = shape(dataSet)
     S = errType(dataSet)
     bestS = inf
@@ -75,3 +97,41 @@ def chooseBestSplit(dataSet, leafType = regLeaf, errType = regErr, ops = [1,
     if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
         return None, leafType(dataSet)
     return bestIndex, bestValue
+
+def isTree(obj):
+    return (type(obj).__name__ == 'dict')
+
+def getMean(tree):
+    if isTree(tree['right']):
+        tree['right'] = getMean(tree['right'])
+    if isTree(tree['left']):
+        tree['left'] = getMean(tree['left'])
+    return (tree['left'] + tree['right']) / 2.0
+
+def prune(tree, testData):
+    if shape(testData)[0] == 0:
+        return getMean(tree)
+
+    if (isTree(tree['right'])) or (isTree(tree['left'])):
+        lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+    if isTree(tree['left']):
+        tree['left'] = prune(tree['left'], lSet)
+    if isTree(tree['right']):
+        tree['right'] = prune(tree['right'], rSet)
+    if not isTree(tree['left']) and not isTree(tree['right']):
+        lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+        errorNoMerge = sum(power(lSet[:, -1] - tree['left'], 2)) + \
+                sum(power(rSet[:, -1] - tree['right'], 2))
+        treeMean = (tree['left'] + tree['right']) / 2.0
+        errorMerge = sum(power(testData[:, -1] - treeMean, 2))
+        if errorNoMerge > errorMerge:
+            print("Merging")
+            return treeMean
+        else:
+            return tree
+    else:
+        return tree
+
+def TestModelTree():
+    myMat = mat(loadDataSet('exp2.txt'))
+    print(createTree(myMat, modelLeaf, modelErr, (1, 10)))
